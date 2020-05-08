2bf543fc9359965b1811d7ba843ee076a39d7116
Dan Santos <ivan.santos.v@mail.pucv.cl>
2020-05-08 15:19:56 -0400

    Se documenta s-box.c y s-box.h

--- c-implementation/src/s-box.c
+++ c-implementation/src/s-box.c
@@ -5,6 +5,11 @@
 
 #define MAX_LINE 50
 
+/**
+ * Carga la tabla s-box del archivo en la ruta ingresada, si hubo un error al abrir el archivo
+ * muestra una amenaza y termina la funcion, sino lee todos los valores del archivo y los 
+ * guarda en la tabla original y la tabla invertida, por ultimo cierra el archivo.
+ * */
 S_box *loadBoxTable(char *path)
 {
 	FILE *file = fopen(path, "r");
@@ -35,9 +40,6 @@ S_box *loadBoxTable(char *path)
 			}
 		}
 
-		showBoxTable(new_table->matrix);
-		//showBoxTable(new_table->inv_matrix);
-
 		fclose(file);
 	}
 	else
@@ -48,6 +50,9 @@ S_box *loadBoxTable(char *path)
 	return new_table;
 }
 
+/**
+ * Muestra la tabla ingresada con el formato correspondiente, si la tabla es nula imprime una advertencia y termina la funcion.
+ * */
 void showBoxTable(int table[RADIX][RADIX])
 {
 	if(table != NULL)
@@ -78,6 +83,12 @@ void showBoxTable(int table[RADIX][RADIX])
 	}	
 }
 
+/**
+ * Funcion que devuelve el valor correspondiente a las coordenadas de "value", el primer digito
+ * de su representacion en hexadecimal corresponde a la fila, y el segundo digito de su representacion
+ * en hexadecimal corresponde a la columna, si estos valores estan en el rango valido devuelve el 
+ * valor correspondiente, sino imprime una advertencia y devuelve -1.
+ * */
 int getValue(int table[RADIX][RADIX], int value)
 {
 	int value_table = -1;
--- c-implementation/src/s-box.h
+++ c-implementation/src/s-box.h
@@ -3,14 +3,26 @@
 #define RADIX 16
 #define MATRIX_ORDER 4
 
+/**
+ * Estructura principal de la tabla s-box
+ * */
 typedef struct S_box {
-	int matrix[RADIX][RADIX];
-	int inv_matrix[RADIX][RADIX];
+	int matrix[RADIX][RADIX];				//tabla original (se usa en el proceso de encriptacion)
+	int inv_matrix[RADIX][RADIX];			//tabla invertida (se usa en el proceso de desencriptacion)
 
 } S_box;
 
+/**
+ * Devuelve una tabla cargada con los valores del archivo de la ruta ingresada.
+ * */
 S_box *loadBoxTable(char *path);
 
+/**
+ * Muestra por pantalla la tabla ingresada, con el formato correspondiente.
+ * */
 void showBoxTable(int table[RADIX][RADIX]);
 
+/**
+ * Devuelve el valor correspondiente a la coordenada (value, 1er digito en hex = filas, 2do digito en hex = columan) ingresada.
+ * */
 int getValue(int table[RADIX][RADIX], int value);
